c9d705fc9d3b7ae1f620482162b609c219f85ff1
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-02 18:36:34 +0700

    Added Product model

---
+++ server/models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
