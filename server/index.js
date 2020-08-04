383928b074f00672c66738809cdf6d85cac53f2c
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-04 17:37:41 +0700

    Used "cors" module for cross-domain requests

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const path = require('path')
 require('./db/mongoose')
@@ -9,6 +10,7 @@ const PORT = process.env.PORT
 const publicDirectoryPath = path.join(__dirname, '../public')
 const app = express()
 
+app.use(cors())
 app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 app.use(adminRouter)
