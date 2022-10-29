9ce56367a6a4e35d5bab0bce404a8bbfa0e72322
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 22:46:32 +0700

    Clean the code of NotFoundPage component

--- client/components/NotFoundPage.js
+++ client/components/NotFoundPage.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => (
-    <div className="page">
-        <h2>404: Not Found</h2>
+  <div className="page">
+    <h2>404: Not Found</h2>
+    <p>How on Earth did you get here?!</p>
+    Go to <Link to="/">home page</Link> or stay here for a while if you're not that hungry
+  </div>
+)
 
-        <p>How on Earth did you get here?!</p>
-
-        Go to <Link to="/">home page</Link> or stay here for a while if you're not that hungry
-    </div>
-);
-
-export default NotFoundPage;
+export default NotFoundPage
