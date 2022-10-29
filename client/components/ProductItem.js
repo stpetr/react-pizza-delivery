2dff100b1562af1c91ffe204b6bd00c03f30bc5c
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-30 00:23:26 +0700

    Refactor the product pages to functional components

--- client/components/ProductItem.js
+++ client/components/ProductItem.js
@@ -1,41 +1,34 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { cartAddItem } from "../actions/cart";
+import { useDispatch, useSelector } from 'react-redux'
+import { cartAddItem } from "../actions/cart"
 import { getAmountInCurrencyFormatted } from '../helpers/amount-in-currency'
 
-export class ProductItem extends React.Component {
-    onAddToCart = (product) => {
-        this.props.cartAddItem(product)
-    }
-    render() {
-        const { product } = this.props
-        return (
-            <div className="product-item">
-                <div className="product-item__top">
-                    { product.image &&
-                    <img src={`${process.env.API_URL}product/${product._id}/image`} className="product-item__image" />
-                    }
-                    <div className="product-item__content">
+export const ProductItem = ({ product }) => {
+  const { currency } = useSelector((state) => state)
+  const dispatch = useDispatch()
 
-                        <h3 className="product-item__title">{product.title}</h3>
-                        <p className="product-item__description">{product.description}</p>
-                    </div>
-                </div>
-                <div className="product-item__bottom">
-                    <button className="btn -pill" onClick={() => this.onAddToCart(product)}>Add to cart</button>
-                    <span className="product-item__price">{getAmountInCurrencyFormatted(product.price, this.props.currency)}</span>
-                </div>
-            </div>
-        )
-    }
+  const onAddToCart = (product) => {
+    dispatch(cartAddItem(product))
+  }
+
+  return (
+    <div className="product-item">
+      <div className="product-item__top">
+        {product.image &&
+          <img src={`${process.env.API_URL}product/${product._id}/image`} className="product-item__image" alt={product.title}/>
+        }
+        <div className="product-item__content">
+
+          <h3 className="product-item__title">{product.title}</h3>
+          <p className="product-item__description">{product.description}</p>
+        </div>
+      </div>
+      <div className="product-item__bottom">
+        <button className="btn -pill" onClick={() => onAddToCart(product)}>Add to cart</button>
+        <span className="product-item__price">{getAmountInCurrencyFormatted(product.price, currency)}</span>
+      </div>
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-    currency: state.currency
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    cartAddItem: (product) => dispatch(cartAddItem(product))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProductItem)
+export default ProductItem
--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,44 +1,36 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import ProductItem from './ProductItem'
 
-class ProductList extends React.Component {
-    render() {
-        const { loading, error, products } = this.props
+export const ProductList = () => {
+  const { loading, items: products, error } = useSelector(({ products }) => products)
 
-        if (error) {
-            return (
-                <div className="product-list__failure">
-                    <p><strong>An error occurred while loading products. Please try again later ;-(</strong></p>
-                    <p>{error}</p>
-                </div>
-            )
-        }
+  if (error) {
+    return (
+      <div className="product-list__failure">
+        <p><strong>An error occurred while loading products. Please try again later ;-(</strong></p>
+        <p>{error}</p>
+      </div>
+    )
+  }
 
-        if (loading) {
-            return (
-                <div className="product-list__loading">
-                    <img src="images/loader.gif" />
-                </div>
-            )
-        }
+  if (loading) {
+    return (
+      <div className="product-list__loading">
+        <img src="images/loader.gif" alt="Loading..."/>
+      </div>
+    )
+  }
 
-        return (
-            <ul className="product-list">
-                {products.map(product =>
-                    <li className="product-list__item" key={product._id}>
-                        <ProductItem product={product} />
-                    </li>
-                )}
-            </ul>
-        )
-    }
+  return (
+    <ul className="product-list">
+      {products.map((product) => (
+        <li className="product-list__item" key={product._id}>
+          <ProductItem product={product}/>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
-const mapStateToProps = state => ({
-    loading: state.products.loading,
-    products: state.products.items,
-    error: state.products.error
-})
-
-export default connect(mapStateToProps)(ProductList)
+export default ProductList
