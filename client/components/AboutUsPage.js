5f2af388c12d03781892e4c2e9bb8bb3af2045a3
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 19:59:56 +0700

    Clean the code of AboutUsPage component

--- client/components/AboutUsPage.js
+++ client/components/AboutUsPage.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const AboutUsPage = () => (
-    <div className="page">
-        <h1>Who we are?</h1>
-        <p>
-            We are one of the best Pizza stores on Earth! Probably even in the Galaxy!
-        </p>
-    </div>
-);
+  <div className="page">
+    <h1>Who we are?</h1>
+    <p>
+      We are one of the best Pizza stores on Earth! Probably even in the Galaxy!
+    </p>
+  </div>
+)
 
-export default AboutUsPage;
+export default AboutUsPage
