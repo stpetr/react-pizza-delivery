0c60cb66db5ecf1e46d1e84552ec0b22d151b7d2
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 19:39:19 +0700

    Refactor HomePage to functional component

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ProductList from "./ProductList";
-import CartWidget from "./CartWidget";
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import ProductList from "./ProductList"
+import CartWidget from "./CartWidget"
 
-export class HomePage extends React.Component {
-    render() {
-        return (
-            <div className="page page__home">
-                <h1 className="page__heading">Welcome to our Pizza restaurant!</h1>
-                <h2 className="page__home__menu-heading">Here's what we've got for you:</h2>
-                <div className="with-sidebar">
-                    <ProductList />
-                    <div className="sidebar">
-                        <CartWidget navigateToOrderPage={() => this.props.history.push('/order')} />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+export const HomePage = () => {
+  const history = useHistory()
+
+  return (
+    <div className="page page__home">
+      <h1 className="page__heading">Welcome to our Pizza restaurant!</h1>
+      <h2 className="page__home__menu-heading">Here's what we've got for you:</h2>
+      <div className="with-sidebar">
+        <ProductList/>
+        <div className="sidebar">
+          <CartWidget navigateToOrderPage={() => history.push('/order')}/>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
