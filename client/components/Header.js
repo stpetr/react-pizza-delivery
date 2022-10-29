9e3f30a792692d5173dbfa8a6e1c61649371da40
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 22:45:12 +0700

    Clean the code of Header component

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,31 +1,38 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import CurrencyList from "./CurrencyList";
+import CurrencyList from "./CurrencyList"
 
-const activeClass = {
-    activeClassName: "is-active"
-}
+const navItems = [
+  {
+    text: 'Home',
+    to: '/',
+    exact: true,
+  },
+  {
+    text: 'Promo',
+    to: '/promo',
+  },
+  {
+    text: 'Delivery',
+    to: '/delivery',
+  },
+  {
+    text: 'About Us',
+    to: '/about',
+  },
+]
 
 export const Header = () => (
-    <header className="header">
-        <div className="content-container">
-            <div className="header__content">
-                <NavLink className="header__link" to="/" exact={true} {...activeClass}>
-                    Home
-                </NavLink>
-                <NavLink className="header__link" to="/promo" {...activeClass}>
-                    Promo
-                </NavLink>
-                <NavLink className="header__link" to="/delivery" {...activeClass}>
-                    Delivery
-                </NavLink>
-                <NavLink className="header__link" to="/about" {...activeClass}>
-                    About Us
-                </NavLink>
-                <CurrencyList />
-            </div>
-        </div>
-    </header>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        {navItems.map(({ text, ...attrs }) => (
+          <NavLink key={text} className="header__link" activeClassName="is-active"  {...attrs}>{text}</NavLink>
+        ))}
+        <CurrencyList/>
+      </div>
+    </div>
+  </header>
 )
 
 export default Header
