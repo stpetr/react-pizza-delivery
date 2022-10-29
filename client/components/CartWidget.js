2b8625b0e5cf615dcda9c1a87e549fa3cbc1e2b0
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 13:55:07 +0700

    Refactor CartWidget to functional component

--- client/components/CartWidget.js
+++ client/components/CartWidget.js
@@ -1,80 +1,91 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { cartIncrementQty, cartDecrementQty, cartRemoveItem, cartClear } from '../actions/cart'
+import React, { useMemo } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { cartIncrementQty, cartDecrementQty, cartRemoveItem } from '../actions/cart'
 import { getAmountInCurrencyFormatted } from '../helpers/amount-in-currency'
-import CurrencyList from "./CurrencyList";
+import CurrencyList from './CurrencyList'
 
-export class CartWidget extends React.Component {
-    getAmount = () => this.props.cart.reduce((amount, item) => amount + item.product.price * item.qty, 0)
-    getProductsTotalQty = () => this.props.cart.reduce((totalQty, item) => totalQty + item.qty, 0)
-    render() {
-        return (
-            <div className="cart-widget">
-                <h3 className="cart-widget__title">
-                    <div>
-                        <span>Cart</span>
-                        { this.props.cart.length > 0 &&
-                            <span className="cart-widget__title-qty">({this.getProductsTotalQty()})</span>
-                        }
-                    </div>
-                    <CurrencyList />
-                </h3>
-                { !this.props.cart.length &&
-                    <div className="cart-widget__empty-message">The cart is empty. Please pick something from our menu</div>
-                }
-                { this.props.cart.length > 0 &&
-                    <div>
-                        <ul className="cart-widget__products">
-                            { this.props.cart.map((item) => (
-                                <li className="cart-widget__products-item" key={item.product._id}>
-                                    <div className="cart-widget__products-item-info">
-                                        <img className="cart-widget__products-item-image" src={`${process.env.API_URL}product/${item.product._id}/image`} />
-                                        <div>
-                                            <div className="cart-widget__products-item-title">{item.product.title}</div>
-                                            <div className="cart-widget__products-item-description">{item.product.description}</div>
-                                        </div>
-                                        <button className="cart-widget__products-item-remove-btn" onClick={() => this.props.cartRemoveItem(item.product)}>&times;</button>
-                                    </div>
+export const CartWidget = ({ navigateToOrderPage }) => {
+  const { cart, currency } = useSelector((state) => state)
+  const dispatch = useDispatch()
 
-                                    <div className="cart-widget__products-item-qty-and-subtotal">
-                                        <div>
-                                            <button
-                                                className="cart-widget__products-item-qty-btn"
-                                                onClick={() => this.props.cartDecrementQty(item.product)}
-                                                disabled={item.qty === 1 ? 'disabled' : ''}
-                                            >&#8722;</button>
-                                            <span className="cart-widget__products-item-qty-between-buttons">{item.qty}</span>
-                                            <button className="cart-widget__products-item-qty-btn" onClick={() => this.props.cartIncrementQty(item.product)}>+</button>
-                                        </div>
-                                        <div>
-                                            {getAmountInCurrencyFormatted(item.product.price * item.qty, this.props.currency)}
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                        <div className="cart-widget__amount">
-                            <span className="cart-widget__amount-title">Total:</span>
-                            <span className="cart-widget__amount-value">{getAmountInCurrencyFormatted(this.getAmount(), this.props.currency)}</span>
-                        </div>
-                        <button className="btn -pill cart-widget__checkout-btn" onClick={this.props.navigateToOrderPage}>Checkout</button>
-                    </div>
-                }
-            </div>
-        )
-    }
+  const productsTotalQty = useMemo(() => {
+    return cart.reduce((acc, item) => acc + item.qty, 0)
+  }, [cart, currency])
+
+  const amount = useMemo(() => {
+    return cart.reduce((acc, item) => acc + item.product.price * item.qty, 0)
+  }, [cart])
+
+  const removeProduct = (product) => {
+    dispatch(cartRemoveItem(product))
+  }
+
+  const decrementProductQty = (product) => {
+    dispatch(cartDecrementQty(product))
+  }
+
+  const incrementProductQty = (product) => {
+    dispatch(cartIncrementQty(product))
+  }
+
+  return (
+    <div className="cart-widget">
+      <h3 className="cart-widget__title">
+        <div>
+          <span>Cart</span>
+          {cart.length > 0 &&
+            <span className="cart-widget__title-qty">({productsTotalQty})</span>
+          }
+        </div>
+        <CurrencyList/>
+      </h3>
+      {!cart.length &&
+        <div className="cart-widget__empty-message">The cart is empty. Please pick something from our menu</div>
+      }
+      {cart.length > 0 &&
+        <div>
+          <ul className="cart-widget__products">
+            {cart.map(({ product, qty }) => (
+              <li className="cart-widget__products-item" key={product._id}>
+                <div className="cart-widget__products-item-info">
+                  <img className="cart-widget__products-item-image"
+                    src={`${process.env.API_URL}product/${product._id}/image`} alt=""/>
+                  <div>
+                    <div className="cart-widget__products-item-title">{product.title}</div>
+                    <div className="cart-widget__products-item-description">{product.description}</div>
+                  </div>
+                  <button className="cart-widget__products-item-remove-btn"
+                    onClick={() => removeProduct(product)}>&times;</button>
+                </div>
+
+                <div className="cart-widget__products-item-qty-and-subtotal">
+                  <div>
+                    <button
+                      className="cart-widget__products-item-qty-btn"
+                      onClick={() => decrementProductQty(product)}
+                      disabled={qty === 1}
+                    >&#8722;</button>
+                    <span className="cart-widget__products-item-qty-between-buttons">{qty}</span>
+                    <button className="cart-widget__products-item-qty-btn"
+                      onClick={() => incrementProductQty(product)}>+
+                    </button>
+                  </div>
+                  <div>
+                    {getAmountInCurrencyFormatted(product.price * qty, currency)}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="cart-widget__amount">
+            <span className="cart-widget__amount-title">Total:</span>
+            <span className="cart-widget__amount-value">{getAmountInCurrencyFormatted(amount, currency)}</span>
+          </div>
+          <button className="btn -pill cart-widget__checkout-btn" onClick={navigateToOrderPage}>Checkout</button>
+        </div>
+      }
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-    cart: state.cart,
-    currency: state.currency,
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    cartIncrementQty: (product) => dispatch(cartIncrementQty(product)),
-    cartDecrementQty: (product) => dispatch(cartDecrementQty(product)),
-    cartRemoveItem: (product) => dispatch(cartRemoveItem(product)),
-    cartClear: () => dispatch(cartClear())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(CartWidget)
+export default CartWidget
