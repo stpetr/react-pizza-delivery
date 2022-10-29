24725f8a72581ea3ec0f4dfe22a497079ac561ee
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 19:37:17 +0700

    Refactor CurrencyList to functional component

--- client/components/CurrencyList.js
+++ client/components/CurrencyList.js
@@ -1,33 +1,27 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { changeCurrency } from '../actions/currency'
 
-export class CurrencyList extends React.Component {
-    isCurrentCurrency = (currency) => {
-        return this.props.currency.currentCurrency.id === currency.id
-    }
-    render() {
-        return (
-            <ul className="currency-list">
-                { this.props.currency.currencyList.map((currency) => (
-                    <li key={currency.id}
-                        className={this.isCurrentCurrency(currency) ? 'is-active' : ''}
-                        onClick={() => this.props.changeCurrency(currency.id)}
-                    >
-                        <span>{currency.sign}</span>
-                    </li>
-                )) }
-            </ul>
-        )
-    }
+export const CurrencyList = () => {
+  const dispatch = useDispatch()
+  const { currencyList, currentCurrency } = useSelector(({ currency }) => currency )
+
+  const handleChangeCurrency = (currencyId) => {
+    dispatch(changeCurrency(currencyId))
+  }
+
+  return (
+    <ul className="currency-list">
+      {currencyList.map(({ id, sign }) => (
+        <li key={id}
+          className={currentCurrency.id === id ? 'is-active' : ''}
+          onClick={() => handleChangeCurrency(id)}
+        >
+          <span>{sign}</span>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
-const mapStateToProps = (state) => ({
-    currency: state.currency
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    changeCurrency: (currencyId) => dispatch(changeCurrency(currencyId))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(CurrencyList)
+export default CurrencyList
