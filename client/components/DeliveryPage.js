0737c2643ddc41922848082e4f903ac6fb2cb3af
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-29 20:01:12 +0700

    Clean the code of DeliveryPage component

--- client/components/DeliveryPage.js
+++ client/components/DeliveryPage.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 const DeliveryPage = () => (
-    <div className="page">
-        <h1>We deliver pizza!</h1>
-        <p>Our delivery service is fast and furious!</p>
-        <p>Some of their mottos are:</p>
-        <ul>
-            <li>In time or never!</li>
-            <li>All or nothing!</li>
-            <li>See the target, see no obstacles!</li>
-            <li>Part of the ship, part of the crew!</li>
-        </ul>
+  <div className="page">
+    <h1>We deliver pizza!</h1>
+    <p>Our delivery service is fast and furious!</p>
+    <p>Some of their mottos are:</p>
+    <ul>
+      <li>In time or never!</li>
+      <li>All or nothing!</li>
+      <li>See the target, see no obstacles!</li>
+      <li>Part of the ship, part of the crew!</li>
+    </ul>
 
-        <p><strong>The delivery costs only $2.5. But if you order for more than $50 we deliver your order for free!</strong></p>
-    </div>
-);
+    <p>
+      <strong>The delivery costs only $2.5. But if you order for more than $50 we deliver your order for free!</strong>
+    </p>
+  </div>
+)
 
-export default DeliveryPage;
+export default DeliveryPage
