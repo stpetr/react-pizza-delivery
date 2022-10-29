dcdf1e128425f5a282c58b290c3c46bd0463b0cc
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2022-10-30 00:24:22 +0700

    Clean the code of PromoPage component

--- client/components/PromoPage.js
+++ client/components/PromoPage.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const PromoPage = () => (
-    <div className="page">
-        <h1>Sometimes we open promo actions, get ready!</h1>
-        <p>
-            No promo actions is currently running :-(
-        </p>
-    </div>
-);
+  <div className="page">
+    <h1>Sometimes we open promo actions, get ready!</h1>
+    <p>
+      No promo actions is currently running :-(
+    </p>
+  </div>
+)
 
-export default PromoPage;
+export default PromoPage
